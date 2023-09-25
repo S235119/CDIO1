5ea4cd9acdd47dfee82377cc559d11a9d30aac26
Rebecca Moss <moss.rebecca.10@gmail.com>
2023-09-25 16:29:32 +0200

    til 40 pint

--- 15_del1/spil.java
+++ 15_del1/spil.java
@@ -1,15 +1,22 @@
 public class spil {
     public static void main(String[] args) {
     var sc = new java.util.Scanner(System.in); 
+    
     System.out.println("Spillet Starter");
-    sc.nextLine();
+    var point = 0;
+    while (point < 40){
+        sc.nextLine();
 
-    var sum = 0;
-    for (int i=0; i<2; i++){
-    int terning = (int) (Math.random()*6+1);
-    sum += terning;
-    }
-    System.out.println(sum);
+        var sum = 0;
+        for (int i=0; i<2; i++){
+            int terning = (int) (Math.random()*6+1);
+            sum += terning;
+        }
+        System.out.println(sum);
+        point += sum;
+        System.out.println("Du er på: " +point+ " point");
+}
+    System.out.println("Spillet er slut du har vundet : )");
     sc.close();
 }
 }
