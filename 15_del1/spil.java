aba934daac4a45906de43b94e597e2fb5fa00f8b
Rebecca Moss <moss.rebecca.10@gmail.com>
2023-09-25 16:04:40 +0200

    To terninger

--- 15_del1/spil.java
+++ 15_del1/spil.java
@@ -1,3 +1,15 @@
 public class spil {
-    
+    public static void main(String[] args) {
+    var sc = new java.util.Scanner(System.in); 
+    System.out.println("Spillet Starter");
+    sc.nextLine();
+
+    var sum = 0;
+    for (int i=0; i<2; i++){
+    int terning = (int) (Math.random()*6+1);
+    sum += terning;
+    }
+    System.out.println(sum);
+    sc.close();
+}
 }
