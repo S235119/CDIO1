c42ee13c60575f9e30e6f1896fd732c96e90f3e9
S235119 <S235119@dtu.dk>
2023-09-26 16:28:34 +0200

    ting med to ens lavet

--- 15_del1/spil.java
+++ 15_del1/spil.java
@@ -20,9 +20,21 @@ public class spil {
 
                 System.out.println(sum1);
                 point1 += sum1;
-                if (terning1 == terning2) {
-                    System.out.println("Du har slået to: " +terning1 + "'ere");
-                }
+                if (sum1 == 2) {
+                    point1 = 0;
+                    System.out.println("Du slog to 1'ere, og mistede alle dine point : (");
+                } else if (terning1 == terning2) {
+                    var tolv = 0;
+                    if (sum1 == 12 && tolv == 12) {
+                        System.out.println("Du slog to 6'ere, to gange i streg");
+                        break;
+                    }
+                    System.out.println("Du har slået to: " +terning1 + "'ere, og du får derfor en ekstra tur");
+                    System.out.println("Spiller 1 er på: " + point1 + " point");
+                    tolv = terning1 + terning2;
+                    continue;
+                } 
+
                 System.out.println("Spiller 1 er på: " + point1 + " point");
                 
             } else {
@@ -31,8 +43,19 @@ public class spil {
 
                 System.out.println(sum2);
                 point2 += sum2;
-                if (terning1 == terning2) {
-                    System.out.println("Du har slået to: " +terning1 + "'ere");
+                if (sum2 == 2) {
+                    point2 = 0;
+                    System.out.println("Du slog to 1'ere, og mistede alle dine point : (");
+                } else if (terning1 == terning2) {
+                    var tolv = 0;
+                    if (sum2 == 12 && tolv == 12) {
+                        System.out.println("Du slog to 6'ere, to gange i streg");
+                        break;
+                    }
+                    System.out.println("Du har slået to: " +terning1 + "'ere, og du får derfor en ekstra tur");
+                    System.out.println("Spiller 2 er på: " + point2 + " point");
+                    tolv = terning1 + terning2;
+                    continue;
                 }
                 System.out.println("Spiller 2 er på: " + point2 + " point");
             }
