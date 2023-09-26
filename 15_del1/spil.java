cd4a5e8e2bbe89c281c20e5b341d1a00f78654a2
S235119 <S235119@dtu.dk>
2023-09-26 15:27:44 +0200

    hver spiller får en tur

--- 15_del1/spil.java
+++ 15_del1/spil.java
@@ -3,19 +3,35 @@ public class spil {
     var sc = new java.util.Scanner(System.in); 
     
     System.out.println("Spillet Starter");
-    var point = 0;
-    while (point < 40){
+        var point1 = 0;
+        var point2 = 0;
+        var i = 0;
+    while (point1 < 40 && point2 < 40){
         sc.nextLine();
+        var sum1 = 0;
+        var sum2 = 0;
 
-        var sum = 0;
-        for (int i=0; i<2; i++){
-            int terning = (int) (Math.random()*6+1);
-            sum += terning;
-        }
-        System.out.println(sum);
-        point += sum;
-        System.out.println("Du er på: " +point+ " point");
-}
+            if (i % 2 == 0) {
+                // spiller 2
+                int terning = (int) (Math.random()*6+1);
+                sum2 += terning;
+
+                System.out.println(sum2);
+                point2 += sum2;
+                System.out.println("Du er på: " + point2 + " point");
+            } else {
+                // spiller 1
+                int terning = (int) (Math.random()*6+1);
+                sum1 += terning;
+
+                System.out.println(sum1);
+                point1 += sum1;
+                System.out.println("Du er på: " + point1 + " point");
+            }
+
+        i++;
+        
+    }
     System.out.println("Spillet er slut du har vundet : )");
     sc.close();
 }
